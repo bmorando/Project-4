aa1c09b90dfb31f219c591ed1c7ce9625f0d6496
Brigette Morando <bibimorando13@yahoo.com>
2016-05-20 18:45:45 -0700

    fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160521010223) do
+ActiveRecord::Schema.define(version: 20160521011956) do
 
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20160521010223) do
     t.string   "songs"
     t.text     "bio"
     t.text     "song_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "music_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
